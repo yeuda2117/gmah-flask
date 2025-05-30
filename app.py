e860848215b51a6a6c36d81fa15594664ccb816f
yeuda2117 <90631819+yeuda2117@users.noreply.github.com>
2025-05-30 13:44:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,20 @@ app = Flask(__name__)
 
 @app.route("/", methods=["POST"])
 def api():
+    # להוציא טקסט שהגיע מהקלטה (סתם בשביל הדגמה)
     text = request.form.get("search_term", "")
-    response_text = "say_api_answer=yes\nid_list_message=t-בדיקה בעברית"
+
+    # תשובה לבדיקה בעברית
+    response_text = "say_api_answer=yes\nid_list_message=t-בדיקה בעברית\n"
+    # או נסה עם אנגלית
+    # response_text = "say_api_answer=yes\nid_list_message=t-Test only\n"
+
+    # חובה! להחזיר כ- windows-1255
     return Response(response_text.encode("windows-1255"), content_type="text/plain; charset=windows-1255")
 
 @app.route("/", methods=["GET"])
 def home():
     return "OK"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
