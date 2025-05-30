2acf651f57f074b6aa79489db73e2b1021d6a36c
yeuda2117 <90631819+yeuda2117@users.noreply.github.com>
2025-05-30 12:11:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ def api():
     response_text = "say_api_answer=yes\nid_list_message=t-שלום! זה בדיקת טקסט בעברית בלבד."
 
     # החזרה בקידוד windows-1255 כפי שדורש ימות המשיח
-    return Response("id_list_message=t-Test only".encode("windows-1255"), content_type="text/plain; charset=windows-1255")
+    return Response("id_list_message=t-Test only", content_type="text/plain")
+
 
 @app.route("/", methods=["GET"])
 def home():
